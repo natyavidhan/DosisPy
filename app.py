641996f5769686d4d604cd3e8be75c6c2ccc668f
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-01-24 08:14:15 +0530

    Added simple authentication

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from models import Database
+from authlib.integrations.flask_client import OAuth
+from loginpass import create_flask_blueprint, GitHub, Google, Gitlab, Discord
+from dotenv import dotenv_values
+
+app = Flask(__name__)
+oauth = OAuth(app)
+
+config = dotenv_values(".env")
+config = dict(config)
+
+app.secret_key = config["SECRET_KEY"]
+for keys in config.keys():
+    app.config[keys] = config[keys]
+    
+database = Database()
+backends = [GitHub, Google, Gitlab, Discord]
+
+@app.route('/')
+def index():
+    if 'user' in session:
+        user = session['user']
+        if user["type"] == "doctor":
+            return render_template('doctor.html', user = user)
+        return render_template('user.html', user = user)
+    return render_template('index.html')
+
+def handle_authorize(remote, token, user_info):
+    if database.userExists(user_info['email']):
+        session['user'] = database.getUserByEmail(user_info['email'])
+    else:
+        database.addUser(user_info['email'])
+        session['user'] = database.getUserByEmail(user_info['email'])
+    return redirect(url_for('index'))
+
+bp = create_flask_blueprint(backends, oauth, handle_authorize)
+app.register_blueprint(bp, url_prefix='/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,71 @@
+from pymongo import MongoClient
+import json
+import os
+import random
+from uuid import uuid4
+from dotenv import dotenv_values
+
+
+class Database:
+    def __init__(self):
+        self.config = dotenv_values(".env")
+        self.db = MongoClient(self.config["MONGOURL"]).Dosis
+        self.users = self.db.users
+        self.blogs = self.db.blogs
+        
+        
+    def makeId(self):
+        return "".join(random.sample("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_", 10))
+    
+    def addUser(self, email):
+        user = {
+            "_id": self.makeId(),
+            "email": email,
+            "name": email.split("@")[0],
+            "blood": "",
+            "age": 0,
+            "contactNumber": 0,
+            "gender": "",
+            "allergies": [],
+            "labResports": [],
+            "medicalReports": [],
+            "notifications": [],
+            "type": "user",
+            "doctors": []
+        }
+        self.users.insert_one(user)
+    
+    def getUserByEmail(self, email):
+        return self.users.find_one({"email": email})
+    
+    def getUser(self, id):
+        return self.users.find_one({"_id": id})
+    
+    def makeDoctor(self, email):
+        self.users.update_one({"email": email}, {"$set": {"type": "doctor"}})
+        self.users.update_one({"email": email}, {"$set": {"patients": []}})
+        
+    def addPatient(self, id, patient):
+        user = self.getUser(id)
+        if user:
+            if user['type'] == "doctor":
+                self.users.update_one({"_id": id}, {"$push": {"patients": patient}})
+            else:
+                return False
+        else:
+            return False
+        return True
+    
+    def addDoctor(self, id, doctor):
+        user = self.getUser(id)
+        if user:
+            if user['type'] == "paient":
+                self.users.update_one({"_id": id}, {"$push": {"doctors": doctor}})
+            else:
+                return False
+        else:
+            return False
+        return True
+    
+    def userExists(self, email):
+        return self.users.find_one({"email": email}) is not None
